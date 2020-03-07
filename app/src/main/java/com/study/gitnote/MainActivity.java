6c4a85db1a3f59ba1cbdb71911c79a9936b93aa9
btazsq <869124558@qq.com>
2020-03-07 11:25:59 +0800

    来着btazsq的更新

--- app/src/main/java/com/study/gitnote/MainActivity.java
+++ app/src/main/java/com/study/gitnote/MainActivity.java
@@ -3,6 +3,8 @@ package com.study.gitnote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btazsq = findViewById(R.id.btazsq_test_button);
+        btazsq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"来自btazsq", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
