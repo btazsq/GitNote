cca931bb27adc6d365d05bd50021e7cc199724f3
王旖 <1226007719@qq.com>
2020-03-07 23:16:18 +0800

    updated by wy

--- app/src/main/java/com/study/gitnote/MainActivity.java
+++ app/src/main/java/com/study/gitnote/MainActivity.java
@@ -3,7 +3,9 @@ package com.study.gitnote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btazsq = findViewById(R.id.btazsq_test_button);
+        //测试冲突
         btazsq.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"来自btazsq", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"来自wy", Toast.LENGTH_SHORT).show();
             }
         });
     }
